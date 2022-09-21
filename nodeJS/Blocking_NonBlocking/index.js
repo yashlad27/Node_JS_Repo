d404c6daa9b696872a19e6d9bbda0dc63866796a
Yash Lad <yashlad727@gmail.com>
2022-09-21 13:12:03 +0530

    Callback hell

--- nodeJS/Blocking_NonBlocking/index.js
+++ nodeJS/Blocking_NonBlocking/index.js
@@ -2,8 +2,23 @@
 const fs = require('fs');
 
 // asynchronous way: node.js starts reading the file in background and will not block the code and immediately move to next line if code
-fs.readFile("/Users/yash/Documents/GitHub/Node_JS_Repo/txt/start.txt", 'utf-8', (err, data) => {
-    console.log(data);
-})
+
+fs.readFile( "/Users/yash/Documents/GitHub/Node_JS_Repo/txt/start.txt", 'utf-8', (err, data1) =>{
+    
+    fs.readFile(`/Users/yash/Documents/GitHub/Node_JS_Repo/txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+            console.log(data2); 
+
+        fs.readFile("/Users/yash/Documents/GitHub/Node_JS_Repo/txt/append.txt", 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('/Users/yash/Documents/GitHub/Node_JS_Repo/txt/final.txt',`${data2}\n${data3}` ,'utf-8', err=> {
+                    console.log("Your file has been written!");
+            });
+        });
+    });
+});
+// fs.readFile("/Users/yash/Documents/GitHub/Node_JS_Repo/txt/start.txt", 'utf-8', (err, data) => {
+//     console.log(data);
+// })
 
 console.log('Will Read file!!')
