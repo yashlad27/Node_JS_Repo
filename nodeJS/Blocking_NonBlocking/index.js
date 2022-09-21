2cb1bfc9c8a8922aa546b3b22e3644b82e5d1ecc
Yash Lad <yashlad727@gmail.com>
2022-09-21 13:01:41 +0530

    Async Function

--- nodeJS/Blocking_NonBlocking/index.js
+++ nodeJS/Blocking_NonBlocking/index.js
@@ -1,7 +1,9 @@
 // Non-blocking, asynchronous way:
 const fs = require('fs');
 
+// asynchronous way: node.js starts reading the file in background and will not block the code and immediately move to next line if code
 fs.readFile("/Users/yash/Documents/GitHub/Node_JS_Repo/txt/start.txt", 'utf-8', (err, data) => {
     console.log(data);
 })
 
+console.log('Will Read file!!')
